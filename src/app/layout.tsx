17fcc827d3b6974f331a85fcb2a40b0e7b8941f1
ATSU <mifutsua@gmail.com>
2023-05-30 23:04:17 +0900

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from 'react'
-import './globals.css'
+// import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
