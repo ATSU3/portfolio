e5ae510fe681299a0cbe4c4e225a07a73b11531b
ATSU <mifutsua@gmail.com>
2023-05-30 22:04:06 +0900

    test

---
+++ src/app/project1/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import React from 'react'
+
+export default function Project1() {
+    return (
+        <div>
+            <h1>Welcome to Project 1!</h1>
+            <Link href="/">/</Link>
+        </div>
+    )
+}
---
+++ src/app/project2/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import React from 'react'
+
+export default function Project2() {
+    return (
+        <div>
+            <h1>Welcome to Project 2!</h1>
+            <Link href="/">/</Link>
+        </div>
+    )
+}
